eed944fe6c28baef1c11cb16b832eca2f8a4ca23
Reece-Sam <bonisamkelekoh@gmail.com>
2025-10-25 14:22:02 +0100

    did the header and some of the hero but i still need to modify them

---
+++ src/Components/Header.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import '../Styles/Header.css';
+
+const Header = () => {
+  return (
+    <div className="header">
+      <img
+        src="src/assets/images/Screenshot 2025-10-23 at 14-28-56 Shopify The All-in-One Commerce Platform for Businesses - Shopify.png "
+        alt="Logo"
+      />
+
+      <ul className="list">
+        <li>Solutions</li>
+        <li>Pricing</li>
+        <li>Resources</li>
+        <li>Enterprise</li>
+        <li>What's new</li>
+
+        <div className="btn">
+          <button className='login'>Log in</button>
+          <button className="start">Start free trial</button>
+        </div>
+      </ul>
+    </div>
+  );
+}
+
+export default Header
---
+++ src/Components/Hero.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "../Styles/Hero.css";
+
+const Hero = () => {
+  return (
+    <div className="video">
+      <video autoPlay muted loop src="src/assets/images/Shopif.webm">
+      </video>
+
+      <div className="content">
+        <h1>Be the next unicorn startup</h1>
+        <p>Dream big,build fast,and grow far on shopify</p>
+        <button className="btn">Start for free</button>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
